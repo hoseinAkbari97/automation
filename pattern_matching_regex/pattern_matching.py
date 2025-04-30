e40ead1c880b34faa06df685de33f006965981d0
Hossein <hossein16akbari@gmail.com>
2025-04-30 22:38:45 +0330

    Test the push

--- pattern_matching_regex/pattern_matching.py
+++ pattern_matching_regex/pattern_matching.py
@@ -1,6 +1,7 @@
 import re
 
 
-def find_phone_number(phone_number)
+def find_phone_number(phone_number):
     US_phone_number_regex = re.compile(r'\b(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}\b')
-    
+    # This is only a comment and code to test the push ability
+    print('Hello World')
